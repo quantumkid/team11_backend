7d33cdcc5efaab9f8bff7211a9385af783783409
Michael Murphy <murphy.md@gmail.com>
2016-11-18 11:39:42 +0000

    Fake get data.

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ var jsonparser = parser.json()
 
 var port = process.env.PORT || 8080
 
-app.get('/', function (req, res) {
-  res.send('Team 11!')
+app.get('/citations', function (req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify([
+    {
+      "text": "Donald Trump is the greatest person ever to have lived.",
+      "cite-url": "https://en.wikipedia.org/wiki/Donald_Trump",
+      "supporting": false
+    },
+    {
+      "text": "The speed of light is really fast",
+      "cite-url": "https://en.wikipedia.org/wiki/Speed_of_light",
+      "supporting": true
+    }
+  ]))
 })
 
 // POST a citation
