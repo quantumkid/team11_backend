3d29e873c606806b489fdac15d6b00db2ad10333
Michael Murphy <murphy.md@gmail.com>
2016-11-18 14:09:32 +0000

    Fixed CORS id.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 8080
 
 // Add headers
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://opcefbhiegghbmomgfleinlclpkeglok');
+    res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://ghkcbjhacmbnmjjdpdolehkkdgpijdpl');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
