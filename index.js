d19b8c9ed9e86228198c600f3d401a2a3d843e11
Michael Murphy <murphy.md@gmail.com>
2016-11-18 13:37:41 +0000

    CORS permissions.

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ var jsonparser = parser.json()
 
 var port = process.env.PORT || 8080
 
+// Add headers
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/citations/:url', function (req, res) {
   db.retrieveCitations(req.params.url).then(function(citations) {
     res.json(citations)
